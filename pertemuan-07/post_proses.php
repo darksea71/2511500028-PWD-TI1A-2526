a1eeb90d1f7b87c526c4d35e2041b54dda9885ae
darksea71 <2511500028@mahasiswa.atmaluhur.ac.id>
2025-11-13 17:44:01 +0700

    sesuaikan get ke post

--- pertemuan-07/post_proses.php
+++ pertemuan-07/post_proses.php
@@ -3,6 +3,6 @@ session_start();
 $_SESSION["nama"] = $_POST["txtNama"];
 $_SESSION["email"] = $_POST["txtEmail"];
 $_SESSION["pesan"] = $_POST["txtPesan"];
-echo $_SESSION["nama"] $_SESSION["email") $_SESSION["pesan"];
+echo $_SESSION["nama"] . $_SESSION["email"] . $_SESSION["pesan"];
 #header("Location: post.php");
 ?>
