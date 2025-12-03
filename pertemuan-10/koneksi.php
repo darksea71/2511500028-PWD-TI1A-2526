091ce675406c15e31ee4f7e4b9a776ded0747c7b
darksea71 <2511500028@mahasiswa.atmaluhur.ac.id>
2025-12-03 12:02:53 +0700

    edit

--- pertemuan-10/koneksi.php
+++ pertemuan-10/koneksi.php
@@ -4,8 +4,8 @@ $user = "root";
 $pass = "";
 $db   = "db_pwd2025";
 
-$conn mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("Koneksi gagal:"mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
