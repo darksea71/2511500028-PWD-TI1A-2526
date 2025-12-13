ff898f00ed5ad2e23cf671660edfb1d648791ab9
darksea71 <2511500028@mahasiswa.atmaluhur.ac.id>
2025-12-13 22:58:22 +0700

    update index.php dengan menambahkan kode untuk menampilkan flash message dari session

--- pertemuan-11/README.md
+++ pertemuan-11/README.md
@@ -6,4 +6,5 @@ Hari ini, Rabu 03 Desember 2025, saya mempelajari:
 <ol>
     <li>copy file folder pertemuan 10</li>
     <li>Update fungsi.php untuk helper redirect sederhana (pola PRG).</li>
+    <li>update index.php dengan menambahkan kode untuk menampilkan flash message dari session</li>
 </ol>
--- pertemuan-11/index.php
+++ pertemuan-11/index.php
@@ -109,22 +109,49 @@ require_once __DIR__ . '/fungsi.php';
       <?= tampilkanBiodata($fieldConfig, $biodata) ?>
     </section>
 
+    <?php
+    $flash_sukses = $_SESSION['flash_sukses'] ?? '';  # jika query sukses
+    $flash_error  = $_SESSION['flash_error'] ?? '';   # jika ada error
+    $old          = $_SESSION['old'] ?? [];           # untuk nilai lama form
+
+    unset($_SESSION['flash_sukses'], $_SESSION['flash_error'], $_SESSION['old']); # bersihkan 3 session ini
+    ?>
+    
     <section id="contact">
       <h2>Kontak Kami</h2>
+      
+      <?php if (!empty($flash_sukses)): ?>
+        <div style="padding:10px; margin-bottom: 10px; background:#d4edda; color: #155724; border-radius:6px;">
+          <?= $flash_sukses; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if (!empty($flash_error)): ?>
+        <div style="padding: 10px; margin-bottom: 10px; background:#f8d7da; color:#721c24; border-radius: 6px;">
+        <?= $flash_error; ?>
+        </div>
+      <?php endif; ?>
+      
       <form action="proses.php" method="POST">
 
         <label for="txtNama"><span>Nama:</span>
-          <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required autocomplete="name">
+          <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama"
+            required autocomplete="name"
+            value="<?= isset($old['nama']) ? htmlspecialchars($old['nama']) : '' ?>">
         </label>
 
         <label for="txtEmail"><span>Email:</span>
-          <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan email" required autocomplete="email">
+          <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan email"
+            required autocomplete="email"
+            value="<?= isset($old['email']) ? htmlspecialchars($old['email']) : '' ?>">
         </label>
 
         <label for="txtPesan"><span>Pesan Anda:</span>
-          <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..." required></textarea>
+          <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..."
+            required><?= isset($old['pesan']) ? htmlspecialchars($old['pesan']) : '' ?></textarea>
           <small id="charCount">0/200 karakter</small>
         </label>
+
 
         <button type="submit">Kirim</button>
         <button type="reset">Batal</button>
