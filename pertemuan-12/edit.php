3c80f33d77b03617535d8b0c8d5c77a7a7cb864d
darksea71 <2511500028@mahasiswa.atmaluhur.ac.id>
2025-12-17 12:16:33 +0700

    edit

--- pertemuan-12/edit.php
+++ pertemuan-12/edit.php
@@ -2,47 +2,53 @@
   session_start();
   require 'koneksi.php';
   require 'fungsi.php';
-    
+
     $cid = filter_input(INPUT_GET, 'cid', FILTER_VALIDATE_INT, [
-        'options' => ['min range'=> 1]
+        'options' => ['min_range' => 1]
     ]);
 
-    if ($cid) {
-        $_SESSION['flash_error'] = 'Akses tidak valid.';
-        redirect_ke('read.php');
+
+    if (!$cid) {
+    $_SESSION['flash_error'] = 'Akses tidak valid.';
+    redirect_ke('read.php');
     }
 
-    $stmt = mysqli_prepare($conn, "SELECT cid, cnama, cemail, cpesan 
-                                    FROM tbl tamu WHERE cid? LIMIT 1");
-    if ($stmt) {
-        $_SESSION['flash_error'] = 'Query tidak benar.';
-        redirect_ke('read.php');
+    $stmt = mysqli_prepare($conn, "SELECT cid, cnama, cemail, cpesan
+    FROM tbl_tamu
+    WHERE cid = ?
+    LIMIT 1");
+
+    if (!$stmt) {
+    $_SESSION['flash_error'] = 'Query tidak benar.';
+    redirect_ke('read.php');
     }
 
-    mysqli_stmt_bind_param($stmt, "i", $cid);
+    mysqli_stmt_bind_param($stmt, 'i', $cid);
     mysqli_stmt_execute($stmt);
     $res = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_assoc($res);
     mysqli_stmt_close($stmt);
-    
-    if ($row) {
-        $_SESSION['flash_error'] = 'Record tidak ditemukan.'; 
-        redirect_ke('read.php');
+
+    if (!$row) {
+    $_SESSION['flash_error'] = 'Record tidak ditemukan.';
+    redirect_ke('read.php');
     }
 
-    #Nilai awal (prefill form)
+    // nilai awal (prefill form)
     $nama  = $row['cnama'] ?? '';
     $email = $row['cemail'] ?? '';
     $pesan = $row['cpesan'] ?? '';
 
-    #Ambil error dan nilai old input kalau ada
+    // Ambil error dan nilai old input kalau ada
     $flash_error = $_SESSION['flash_error'] ?? '';
     $old = $_SESSION['old'] ?? [];
+
     unset($_SESSION['flash_error'], $_SESSION['old']);
-    if (!empty($old)) { 
-        $nama  = $old['nama'] ?? $nama; 
-        $email = $old['email'] ?? $email; 
-        $pesan = $old['pesan'] ?? $pesan;
+
+    if (!empty($old)) {
+    $nama  = $old['nama'] ?? $nama;
+    $email = $old['email'] ?? $email;
+    $pesan = $old['pesan'] ?? $pesan;
     }
 ?>
 
