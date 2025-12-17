12532f1be588f9d1d9c59a89b3c52b4be5073ad3
darksea71 <2511500028@mahasiswa.atmaluhur.ac.id>
2025-12-17 11:14:55 +0700

    penampil flash sukses maupun flash error menggunakan session pada read.php

--- pertemuan-12/read.php
+++ pertemuan-12/read.php
@@ -1,9 +1,35 @@
 <?php
-require 'koneksi.php';
+  session_start();
+  require 'koneksi.php';
+  require 'fungsi.php';
 
 $sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
 $q = mysqli_query($conn, $sql);
+  if (!$q) {
+  die("Query error: " . mysqli_error($conn));
+  }
 ?>
+
+<?php
+  $flash_sukses = $_SESSION['flash_sukses'] ?? ''; #jika query sukse
+  $flash_error = $_SESSION['flash_error'] ?? ''; #jika ada error
+  #bersihkan session ini
+  unset($_SESSION['flash_sukses'], $_SESSION['flash_error']);
+?>
+
+<?php if (!empty($flash_sukses)): ?>
+        <div style="padding:10px; margin-bottom:10px;
+          background: #d4edda; color:#155724; border-radius: 6px;">
+          <?= $flash_sukses; ?>
+        </div>
+<?php endif; ?>
+
+<?php if (!empty($flash_error)): ?>
+        <div style="padding:10px; margin-bottom:10px;
+          background:#f8d7da; color:#721c24; border-radius: 6px;">
+          <?= $flash_error; ?>
+        </div>
+<?php endif; ?>
 
 <table border="1" cellpadding="8" cellspacing="0">
   <tr>
